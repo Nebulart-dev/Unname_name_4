7c35882b4c8a7e668eb8e28bf7c3b0c8e45592b7
/artem kasapov <kasapova79@gmail.com>
2024-11-06 18:02:54 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,135 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Рисунок в консоле (Прямоугольник)
+/*
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 1; i < 8; i++) {
+            System.out.println("* * * * *");
         }
     }
 }
+*/
+
+/*
+//Рисунок в консоле (Треугольник - Полный)
+public class Main {
+    public static void main(String[] args) {
+        int count = 7;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (j < count - i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+/*
+//Рисунок в консоле (Треугольник - Полутреугольник 1)
+public class Main {
+    public static void main(String[] args) {
+        int count = 10;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (j > count - i - 2) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+/*
+//Рисунок в консоле (Треугольник - Полутреугольник 2)
+public class Main {
+    public static void main(String[] args) {
+        int count = 10;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (j < count - i) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+/*
+//Рисунок в консоле (Ромбик)
+public class Main {
+    public static void main(String[] args) {
+        int count = 7;
+        int n = count;
+
+        //Верхняя часть
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (j < count - i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+        //Нижняя часть
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < count; j++) {
+                if (j > count - i - 1) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+*/
+
+/*
+//Рисунок в консоле (Слеш 1)
+public class Main {
+    public static void main(String[] args) {
+        int count = 10;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (j == count - i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
+
+/*
+//Рисунок в консоле (Слеш 2)
+public class Main {
+    public static void main(String[] args) {
+        int count = 10;
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                if (j == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+*/
